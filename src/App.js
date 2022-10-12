bb5aa36918bdeec0820a35b1ee336543e6c4ad78
bakytt88 <36629741+bakytt88@users.noreply.github.com>
2022-10-12 16:59:48 +0600

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from "../../hooks/useTelegram";
+import { useTelegram } from './hooks/useTelegram';
+
 
 function App() {
 
